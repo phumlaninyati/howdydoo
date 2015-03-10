2c0a2f1c2a99a83dfdbfd844b40721d7cc587806
Ovayo <montiovayo@gmail.com>
2015-03-10 12:53:35 +0200

    staff pictures

--- js/validation.js
+++ js/validation.js
@@ -41,3 +41,24 @@ $('#staff_form').validate({
             $(element).closest('.form-group').removeClass('has-error');
         }
     });
+
+function muModal(staff_form)
+{
+  var form=staff_form,
+      modal=$('<div/>', {
+    'id':'alert',
+  })
+  .dialog({
+    'title':'Iframe in a modal window',
+    'modal':true,
+    'width':350,
+    'height':'auto',
+    'buttons': {
+      'OK': function() { 
+        $(this).dialog( "close" ); 
+        // do something, maybe call form.submit();
+      }
+     }
+  });
+  return false;
+}
